f155a5e288f655efb70c660f3d6e3affde987ede
bumble-beed <shirleen.workstuff@gmail.com>
2026-08-10 22:05:52 +1000

    Add loop

--- _printf.c
+++ _printf.c
@@ -1,18 +1,33 @@
 #include "main.h"
 
 /**
-* _printf - Prints
+* _printf - Read format and product output
 *
 * Return: anything
 */
 
 int _printf(const char *format, ...)
 {
-	va_list ap;
-	
-	va_start(ap, format);
+	va_list position;
+	int i;
 
-	va_end(ap);
+	va_start(position, format);
+	i = 1;
+
+	while (format[i] != '\0')
+	{
+		if (format[i] == '%')
+		{
+			i = -1;
+		}
+		else
+		{
+			i = 1;
+		}
+	i++;
+	}
+
+	va_end(position);
 
 	return(0);
 }
