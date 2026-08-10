da460286fd216b973723a79087dcd1e71ccb65d6
bumble-beed <shirleen.workstuff@gmail.com>
2026-08-10 20:45:33 +1000

    Add _print.c file

---
+++ _printf.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+* _printf - Prints
+*
+* Return: anything
+*/
+
+int _printf(const char *format, ...)
+{
+	va_list args;
+
+	va_start(args);
+	va_end(args);
+
+	return(0);
+}
